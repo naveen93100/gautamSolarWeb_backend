42745fc847349d855df3d918b43eb6eb1dc9727e
naveen93100 <naveen9310097505@gmail.com>
2025-12-26 13:41:59 +0530

    change mailing service

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,13 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const dealerTransporter = nodemailer.createTransport({
-  host: 'smtp.hostinger.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.DEALER_MAIL,
-    pass: process.env.DEALER_PASS,
-  },
+  service:'gmail',
+  auth:{
+    user:process.env.DEALER_MAIL,
+    pass:process.env.DEALER_PASS
+  }
 });
 
 module.exports=dealerTransporter;
