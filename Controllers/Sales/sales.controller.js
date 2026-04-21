242d94a34ae327ed7e71e255b339b55de22ff7aa
naveen93100 <naveen9310097505@gmail.com>
2026-04-21 15:59:11 +0530

    add new api

--- Controllers/Sales/sales.controller.js
+++ Controllers/Sales/sales.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Sales = require("../../Models/Sales/sales.schema");
 
 const createSalesPerson = async (req, res) => {
@@ -35,6 +36,73 @@ const createSalesPerson = async (req, res) => {
   }
 };
 
+const updateSalesAccount = async (req, res) => {
+  try {
+    let { salesId, name, email, phone } = req.body;
+
+    if (!mongoose.isValidObjectId(salesId))
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or missing Id." });
+    let salesAccount = await Sales.findOne({ _id: salesId });
+
+    if (!salesAccount)
+      return res
+        .status(404)
+        .json({ success: false, message: "Account not found!" });
+
+    let newData = {};
+
+    if (name && name.trim()) {
+      newData.name = name.trim();
+    }
+
+    if (email) {
+      email = email.trim().toLowerCase();
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid email format",
+        });
+      }
+      
+      if (email !== salesAccount.email) {
+        newData.email = email;
+      }
+    }
+
+    if (phone) {
+      phone = phone.replace(/\D/g, "");
+      if (!/^[6-9]\d{9}$/.test(phone)) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid Phone number!" });
+      }
+      if (phone !== salesAccount.phone) {
+        newData.phone = phone;
+      }
+    }
+
+    if (Object.keys(newData).length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No Changes provided!" });
+    }
+
+    await Sales.findByIdAndUpdate(salesId, { $set: newData });
+    return res.status(200).json({ success: true, message: "Account Updated." });
+  } catch (er) {
+    if (er?.code === 11000) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Email or phone already exist" });
+    }
+    return res.status(500).json({ success: false, message: er?.message });
+  }
+};
+
 const getSalesPersonList = async (req, res) => {
   try {
     const sales = await Sales.find({});
@@ -45,20 +113,22 @@ const getSalesPersonList = async (req, res) => {
   }
 };
 
-const createClient=async(req,res)=>{
-     try {
-        
-        const {salesId,name,email,phone,address,companyName,gst}=req.body;
+const createClient = async (req, res) => {
+  try {
+    const { salesId, name, email, phone, address, companyName, gst } = req.body;
 
-        
-
-     } catch (er) {
-        return res.status(500).json({success:false,message:er?.message});
-     }
-}
+    if (!mongoose.isValidObjectId(salesId))
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or missing sales id" });
+  } catch (er) {
+    return res.status(500).json({ success: false, message: er?.message });
+  }
+};
 
 module.exports = {
   createSalesPerson,
   getSalesPersonList,
   createClient,
+  updateSalesAccount,
 };
--- Routes/sales.routes.js
+++ Routes/sales.routes.js
@@ -3,12 +3,15 @@ const {
   createSalesPerson,
   getSalesPersonList,
   createClient,
+  updateSalesAccount,
 } = require("../Controllers/Sales/sales.controller.js");
 
 const router = Router();
 
 router.get('/',getSalesPersonList); // need to add adminAuth middleware here 
 router.post("/create-account", createSalesPerson);
+
+router.patch("/update-account",updateSalesAccount);
 
 router.post('/create-client',createClient);
 
